64ce004d4d4e32ea0764a194631c81fb79af75d0
cspark0 <cspark0@gmail.com>
2024-09-21 17:38:18 +0900

    update ResourceFilter and EncodingFilter

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
-	// request를 처리한 후 이동할 URL을 반환
+	// request를 처리한 후 이동할 JSP의 경로 또는 redirection 주소를 반환
     public String execute(HttpServletRequest request, HttpServletResponse response) 
     		throws Exception;
 }
