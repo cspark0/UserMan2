47275a011dba37ce2f5f62e74d951a6aa71e1fee
cspark0 <cspark0@gmail.com>
2024-09-22 22:36:35 +0900

    share UserManager in UpdateUserController

--- src/main/java/controller/user/UpdateUserController.java
+++ src/main/java/controller/user/UpdateUserController.java
@@ -17,6 +17,8 @@ public class UpdateUserController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
  
+    	UserManager manager = UserManager.getInstance();
+		
     	if (request.getMethod().equals("GET")) {	
     		// GET request: 회원정보 수정 form 요청	
     		// 원래는 UpdateUserFormController가 처리하던 작업을 여기서 수행
@@ -24,8 +26,7 @@ public class UpdateUserController implements Controller {
 
     		log.debug("UpdateForm Request : {}", updateId);
     		
-    		UserManager manager = UserManager.getInstance();
-			User user = manager.findUser(updateId);	// 수정하려는 사용자 정보 검색
+    		User user = manager.findUser(updateId);	// 수정하려는 사용자 정보 검색
 			request.setAttribute("user", user);			
 
 			HttpSession session = request.getSession();
@@ -53,7 +54,6 @@ public class UpdateUserController implements Controller {
 
     	log.debug("Update User : {}", updateUser);
 
-		UserManager manager = UserManager.getInstance();
 		manager.update(updateUser);			
         return "redirect:/user/list";			
     }
